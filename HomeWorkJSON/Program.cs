d6a2d8a9edc8e76bb0041c860f3463f640ce23e5
Radosław Łodej <radoslawlodej1991@gmail.com>
2022-02-15 14:11:50 +0100

    Add project files.

---
+++ HomeWorkJSON/Program.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using Newtonsoft.Json;
+
+var client = new RestClient("https://restcountries.com/v3.1");
+
+var request = new RestRequest("all");
+var response = await client.GetAsync(request);
+
+
+List<Country> countryList = JsonConvert.DeserializeObject<List<Country>>(response.Content);
+
+Console.WriteLine("");
+
+
+
+class Country
+{
+    public Name name { get; set; }
+    public string region { get; set; }
+    public string subregion { get; set; }
+    public int population { get; set; }
+
+    public class Name
+    {
+        [JsonProperty("common")]
+        public string Common { get; set; }
+        [JsonProperty("official")]
+        public string Official { get; set; }
+    }
+}
+
