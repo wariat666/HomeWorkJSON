7d6728aebea3ab53384a72d412438a00c9dd44d7
Radosław Łodej <radoslawlodej1991@gmail.com>
2022-02-23 17:58:02 +0100

    Bot najnowsza wersja po ostatniej nauce

---
+++ HomeWorkJSON/CommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace HomeWorkJSON
+{
+   public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        // Retrieve client and CommandService instance via ctor
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+
+        public async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+           
+            // Don't process the command if it was a system message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+
+            // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+            {
+                return;
+            }
+
+            // Create a WebSocket-based command context based on the message
+
+            var context = new SocketCommandContext(_client, message);
+
+            // Execute the command with the command context we just
+            // created, along with the service provider for precondition checks.
+            var result = await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong with executing a command. Text: {context.Message.Content} | Error: {result.ErrorReason}");
+            }
+        }
+
+    }
+}
--- HomeWorkJSON/Program.cs
+++ HomeWorkJSON/Program.cs
@@ -1,31 +1,101 @@
-﻿using RestSharp;
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using HomeWorkJSON;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 using Newtonsoft.Json;
 
-var client = new RestClient("https://restcountries.com/v3.1");
-
-var request = new RestRequest("all");
-var response = await client.GetAsync(request);
-
-
-List<Country> countryList = JsonConvert.DeserializeObject<List<Country>>(response.Content);
-
-Console.WriteLine("");
-
-
-
-class Country
+public class Program
 {
-    public Name name { get; set; }
-    public string region { get; set; }
-    public string subregion { get; set; }
-    public int population { get; set; }
+	public static Task Main(string[] args) => new Program().MainAsync();
 
-    public class Name
+	private Task Log(LogMessage logMessage)
+	{
+        if (logMessage.Exception is CommandException cmdException)
+        {
+            // We can tell the user that something unexpected has happened
+            cmdException.Context.Channel.SendMessageAsync("Something went catastrophically wrong!");
+
+            // We can also log this incident
+            Console.WriteLine($"{cmdException.Context.User} failed to execute '{cmdException.Command.Name}' in {cmdException.Context.Channel}.");
+            Console.WriteLine(cmdException.ToString());
+        }
+        Console.WriteLine(logMessage.ToString());
+		return Task.CompletedTask;
+	}
+
+    private DiscordSocketClient Client;
+    private CommandService Commands;
+    private CommandHandler CommandHandler;
+    private ConfigObject ConfigObject;
+    private readonly ServiceProvider _serviceProvider = ServiceProviderUtilities.ConfigureServices();
+    public async Task MainAsync()
     {
-        [JsonProperty("common")]
-        public string Common { get; set; }
-        [JsonProperty("official")]
-        public string Official { get; set; }
+        
+        Client = new DiscordSocketClient(new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Debug
+        });
+
+        ConfigObject = JsonConvert.DeserializeObject<ConfigObject>(File.ReadAllText("test.txt"));
+
+        Commands = new CommandService(new CommandServiceConfig
+        {
+            CaseSensitiveCommands = false,
+            DefaultRunMode = RunMode.Async,
+            LogLevel = LogSeverity.Debug
+        });
+
+        await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+
+        CommandHandler = new CommandHandler(Client, Commands);
+
+        Client.Log += Log;
+        Client.Ready += Client_Ready;
+        Client.MessageReceived += Client_MessageReceived;
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = ConfigObject.botToken;
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+        await Client.LoginAsync(TokenType.Bot, token);
+        await Client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+    private async Task Client_Ready()
+    {
+        await Client.SetGameAsync(ConfigObject.game, "https://www.google.com/");
+    }
+    private async Task Client_MessageReceived(SocketMessage MessageParam)
+    {
+        await CommandHandler.HandleCommandAsync(MessageParam);
+    }
+    public class ServiceProviderUtilities
+    {
+        public static ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<InfoModule>()
+                .BuildServiceProvider();
+        }
     }
 }
+public class ConfigObject
+{
+    public string botToken { get; set; }
+    public string game { get; set; }    
+}
 
+/*
+var client = new RestClient("https://restcountries.com/v3.1");
+var request = new RestRequest("all");
+var response = await client.GetAsync(request);
+*/
